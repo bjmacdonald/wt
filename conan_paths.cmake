029ea550cbd7c3978b6130f7e12afeba7c59f27f
Bruce MacDonald <bruce@bkmacd.com>
2019-05-07 09:26:03 +0100

    New CMake Build WIP

---
+++ conan_paths.cmake
@@ -0,0 +1,15 @@
+set(CONAN_BOOST_ROOT "C:/Users/bruce/.conan/data/boost/1.69.0/capitis/stable/package/22391348a352e18342ae280b95deb8585d779729")
+set(CONAN_WIN64-OPENSSL_ROOT "C:/Users/bruce/.conan/data/win64-openssl/1.0.2k/capitis/stable/package/ca33edce272a279b24f87dc0d4cf5bbdcffbc187")
+set(CONAN_BZIP2_ROOT "C:/Users/bruce/.conan/data/bzip2/1.0.6/capitis/stable/package/571131db0a881e5643f7b2d5860e42e7a5f17e37")
+set(CONAN_ZLIB_ROOT "C:/Users/bruce/.conan/data/zlib/1.2.11/capitis/stable/package/571131db0a881e5643f7b2d5860e42e7a5f17e37")
+set(CONAN_CONANCMAKE_ROOT "C:/Users/bruce/.conan/data/conancmake/1.0/capitis/stable/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9")
+set(CMAKE_MODULE_PATH "C:/Users/bruce/.conan/data/boost/1.69.0/capitis/stable/package/22391348a352e18342ae280b95deb8585d779729/"
+			"C:/Users/bruce/.conan/data/win64-openssl/1.0.2k/capitis/stable/package/ca33edce272a279b24f87dc0d4cf5bbdcffbc187/"
+			"C:/Users/bruce/.conan/data/bzip2/1.0.6/capitis/stable/package/571131db0a881e5643f7b2d5860e42e7a5f17e37/"
+			"C:/Users/bruce/.conan/data/zlib/1.2.11/capitis/stable/package/571131db0a881e5643f7b2d5860e42e7a5f17e37/"
+			"C:/Users/bruce/.conan/data/conancmake/1.0/capitis/stable/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/" ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
+set(CMAKE_PREFIX_PATH "C:/Users/bruce/.conan/data/boost/1.69.0/capitis/stable/package/22391348a352e18342ae280b95deb8585d779729/"
+			"C:/Users/bruce/.conan/data/win64-openssl/1.0.2k/capitis/stable/package/ca33edce272a279b24f87dc0d4cf5bbdcffbc187/"
+			"C:/Users/bruce/.conan/data/bzip2/1.0.6/capitis/stable/package/571131db0a881e5643f7b2d5860e42e7a5f17e37/"
+			"C:/Users/bruce/.conan/data/zlib/1.2.11/capitis/stable/package/571131db0a881e5643f7b2d5860e42e7a5f17e37/"
+			"C:/Users/bruce/.conan/data/conancmake/1.0/capitis/stable/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/" ${CMAKE_PREFIX_PATH} ${CMAKE_CURRENT_LIST_DIR})
---
+++ conanfile.py
@@ -0,0 +1,44 @@
+#  Adapted from awesome https://github.com/Orphis/boost-cmake.git
+import os
+from conans import ConanFile, CMake, tools, python_requires
+
+base = python_requires("conanpy/1.0@capitis/stable")
+
+class ConanFileImpl(base.ConanCMake):
+    name = "wt"
+    version = "4.0.5"
+  
+    def requirements(self):
+        self.requires("conancmake/1.0@capitis/stable")
+        self.requires("boost/1.69.0@capitis/stable")
+        #self.requires("win64-db2/11.1.1010.160@capitis/stable")
+        self.requires("win64-openssl/1.0.2k@capitis/stable")
+        #self.requires("spdlog/1.3.1@capitis/stable")
+        self.requires("zlib/1.2.11@capitis/stable")
+
+    def build_requirements(self):
+        #if self.settings.os == "Windows":
+        #   self.build_requires("ToolWin/0.1@user/stable")
+        pass
+
+    def system_requirements(self):
+        #self.global_system_requirements=True
+        #pack_name = None
+        #if os_info.linux_distro == "ubuntu":
+        #   if os_info.os_version > "12":
+        #      pack_name = "package_name_in_ubuntu_10"
+        #   else:
+        #      pack_name = "package_name_in_ubuntu_12"
+        #elif os_info.linux_distro == "fedora" or os_info.linux_distro == "centos":
+        #   pack_name = "package_name_in_fedora_and_centos"
+        #elif os_info.is_macos:
+        #   pack_name = "package_name_in_macos"
+
+        #if pack_name:
+        #   installer = SystemPackageTool()
+        #   installer.install(pack_name) # Install the package, will update the package database if pack_name isn't already installed
+        pass
+         
+    # def imports(self):
+        # self.copy("*.dll", dst="bin", src="bin") # From bin to bin
+        # #self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
